b86168db201b864470b44c5ecbdc375835b64122
TuanHao2604 <nguyentuanhao2604@gmail.com>
2024-09-09 20:15:14 +0700

    Add files via upload

--- DuongNhoNhat_khoa3.java
+++ DuongNhoNhat_khoa3.java
@@ -4,38 +4,38 @@ public class DuongNhoNhat_khoa3 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a[] = InPut(scan);
+		float a[] = InPut(scan);
 		OutPut(a);
 		int temp = VitriDuongNhoNhat(a);
 		if (temp == -1)
-			System.out.println(temp);
+			System.out.print(temp);
 		else
 			System.out.println("Vị trí " + temp + " có giá trị dương nhỏ nhất đầu tiên ");
 	}
 
-	public static int[] InPut(Scanner scan) {
+	public static float[] InPut(Scanner scan) {
 		System.out.println("Nhập vào số phần phần tử của mảng ");
 		int n;
 		do {
-			System.out.println("Nhập n>0 ");
+			System.out.print("Nhập n>0 ");
 			 n=Integer.parseInt(scan.nextLine());
 		}while( n <=0);
-		int a[]=new int[n];
+		float a[]=new float[n];
 		for(int i =0;i<n;i++) {
-			System.out.println("a["+i+"]: ");
-			a[i]=Integer.parseInt(scan.nextLine());
+			System.out.print("a["+i+"]: ");
+			a[i]=Float.parseFloat(scan.nextLine());
 		}
 		return a;
 	}
 	
-	public static void OutPut(int a[]) {
+	public static void OutPut(float a[]) {
 		for(int i=0;i<a.length;i++) {
 			System.out.print("a["+i+"]:"+a[i]+"\t\t");
 		}
 		System.out.println();
 	}
 
-	public static int DuongDautien(int a[]) {
+	public static int DuongDautien(float a[]) {
 		for(int i=0;i<a.length;i++) {
 			if(a[i]>0)
 				return i;
@@ -43,11 +43,11 @@ public class DuongNhoNhat_khoa3 {
 		return -1;
 	}
 
-	public static int VitriDuongNhoNhat(int a[]) {
+	public static int VitriDuongNhoNhat(float a[]) {
 		int index=DuongDautien(a);
 		if(index==-1)
 			return -1;
-		int Min=a[index];
+		float Min=a[index];
 		for (int i =index; i < a.length; i++) {
 			if (a[i] < Min && a[i] > 0) {
 				Min = a[i];
